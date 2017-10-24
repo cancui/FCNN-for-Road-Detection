acf6a903e856e8c5658173fe8fb07c9dd7523f5d
Can Cui <can.cui888@gmail.com>
2017-10-24 00:15:55 -0700

    added basic model

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 import data
-# from segmenter import Segmenter
+from segmenter import Segmenter
 
 def visualise_results(images, originals, randomise=True):
     if randomise:
@@ -22,9 +22,9 @@ def visualise_results(images, originals, randomise=True):
 if __name__ == '__main__':
     train_images, train_labels = data.read_train_images_and_labels()
 
-    # segmenter = Segmenter(learing_rate=0.001)
+    segmenter = Segmenter(learing_rate=0.001)
 
-    # segmenter.train(train_images, train_labels)
+    segmenter.train(np.asarray([train_images[0]]), np.asarray([train_labels[0]]))
     # segmenter.save_model(path='model1.h5')
 
     # test_images = data.read_test_images()
--- segmenter.py
+++ segmenter.py
@@ -1,17 +1,29 @@
 import keras
+from keras.models import Sequential
+from keras.layers.convolutional import Conv2D, MaxPooling2D, UpSampling2D
+from keras import optimizers
+
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 
 def fully_conv_net(learing_rate=0.001):
-    return
+    model = Sequential(layers=[
+        Conv2D(8, (7,7), strides=(1,1), padding='same', activation='relu', input_shape=(None, None, 3)),
+        Conv2D(8, (7,7), strides=(1,1), padding='same', activation='relu'),
+        Conv2D(3, (3,3), strides=(1,1), padding='same', activation='relu'),
+    ])
+
+    model.compile(optimizer=optimizers.Adam(), loss='categorical_crossentropy', metrics=[])
+
+    return model
 
 class Segmenter(object):
     def __init__(self, learing_rate=0.001):
         self.model = fully_conv_net(learing_rate=learing_rate)
 
     def train(self, features, labels):
-        return
+        self.model.fit(features, labels)
 
     def infer(self, features):
         return
@@ -24,3 +36,4 @@ class Segmenter(object):
 
 if __name__ == '__main__':
     print('Running segmenter.py')
+    segmenter = Segmenter()
