83bfac2bc2d334ae5d6523c106c2ea5276dd3866
Can Cui <ccui@fitbit.com>
2017-11-05 18:26:20 -0800

    remove bash scripts

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import pickle
 from sklearn.model_selection import KFold, train_test_split
 import numpy as np
 import scipy as sp
 import random, time
+from keras.models import load_model
 
 import data
 from segmenter import Segmenter
@@ -48,6 +50,31 @@ def kfold_testing(segmenter_function, all_images, all_labels, splits=5):
 
     return p_acc_sum, m_acc_sum, m_IU_sum, fw_IU_sum
 
+def kfold_data_creation(all_images, all_labels, splits=5):
+    kfold = KFold(n_splits=splits, shuffle=True)
+
+    for i, (train_index, test_index) in enumerate(kfold.split(all_images)):
+        print('FOLD {} ######################'.format(i+1))
+        train_images, test_images = all_images[train_index], all_images[test_index]
+        train_labels, test_labels = all_labels[train_index], all_labels[test_index]
+
+        fold_data = {
+            'train_images' : train_images,
+            'train_labels' : train_labels,
+            'test_images' : test_images,
+            'test_labels' : test_labels
+        }
+
+        with open('fold{}_data.p'.format(i+1), 'wb') as f:
+            pickle.dump(fold_data, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+def load_pickled_fold(num):
+    path = 'fold{}_data.p'.format(num)
+    with open(path, 'rb') as f:
+        fold_data = pickle.load(f)
+
+    return fold_data['train_images'], fold_data['train_labels'], fold_data['test_images'], fold_data['test_labels']
+
 def single_testing(segmenter_function, all_images, all_labels):
     train_images, test_images, train_labels, test_labels = train_test_split(all_images, all_labels, test_size=0.2, shuffle=True)
 
@@ -62,8 +89,40 @@ def single_testing(segmenter_function, all_images, all_labels):
 
     return p_acc, m_acc, m_IU, fw_IU
 
+def test_folds(num=5):
+    p_acc_sum, m_acc_sum, m_IU_sum, fw_IU_sum = 0, 0, 0, 0
+    save_string = ''
+
+    for i in range(num):
+        segmenter = Segmenter()
+        segmenter.model = load_model('saved_models/fold{}.h5'.format(i+1))
+        train_images, train_labels, test_images, test_labels = load_pickled_fold(i+1)
+
+        p_acc, m_acc, m_IU, fw_IU = segmenter.test_model(test_images, test_labels)
+        result_string = 'For this fold:    pixel_acc:{}, mean_acc:{}, mean_IU:{}, freq_IU:{}\n'.format(p_acc, m_acc, m_IU, fw_IU)
+        print(result_string)
+        save_string += result_string
+
+        p_acc_sum += p_acc
+        m_acc_sum += m_acc
+        m_IU_sum += m_IU
+        fw_IU_sum += fw_IU
+
+    p_acc_sum, m_acc_sum, m_IU_sum, fw_IU_sum = p_acc_sum/num, m_acc_sum/num, m_IU_sum/num, fw_IU_sum/num
+    
+    overall_string = 'Overall:    pixel_acc:{}, mean_acc:{}, mean_IU:{}, freq_IU:{}\n'.format(p_acc_sum, m_acc_sum, m_IU_sum, fw_IU_sum)
+    print(overall_string)
+    save_string += overall_string
+
+    with open('results_' + time.strftime("%m-%dth_%H:%M:%S") + '.txt', 'w') as f:
+        f.write(save_string)
+
+    return p_acc_sum, m_acc_sum, m_IU_sum, fw_IU_sum
+
 if __name__ == '__main__':
-    train_images, train_labels = data.read_train_images_and_labels()
+    # train_images, train_labels = data.read_train_images_and_labels()
+
+    # kfold_data_creation(train_images, train_labels, splits=5)
 
     # kfold_testing(Segmenter, train_images, train_labels, splits=5)
     # single_testing(Segmenter, train_images, train_labels)
@@ -73,9 +132,23 @@ if __name__ == '__main__':
     # visualise_results(train_labels, train_images, randomise=False, num=1)
     # exit()
 
-    segmenter = Segmenter()
 
-    segmenter.train(train_images, train_labels)
+
+    # train_images, train_labels, test_images, test_labels = load_pickled_fold(4)
+    # segmenter = Segmenter(save_name='fold4.h5')
+    # segmenter.train(train_images, train_labels, test_images, test_labels)
+
+    # p_acc, m_acc, m_IU, fw_IU = segmenter.test_model(test_images, test_labels)
+    # result_string = 'For this fold:    pixel_acc:{}, mean_acc:{}, mean_IU:{}, freq_IU:{}\n'.format(p_acc, m_acc, m_IU, fw_IU)
+    # print(result_string)
+
+    # with open('results_' + time.strftime("%m-%dth_%H:%M:%S") + '.txt', 'w') as f:
+    #     f.write(result_string)
+
+
+
+    test_folds()
+
 
     # segmenter.load_model('saved_models/vgg4.h5')
 
--- segmenter.py
+++ segmenter.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy as sp
+import keras
 from keras.utils import plot_model
 from keras.applications.vgg16 import VGG16
 from keras.applications.inception_v3 import InceptionV3
@@ -8,11 +9,74 @@ from keras.models import Sequential, Model, load_model
 from keras.layers import Input, Dropout, Add, BatchNormalization
 from keras.layers.convolutional import Conv2D, MaxPooling2D, UpSampling2D, Conv2DTranspose
 from keras import optimizers
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
 
 import time
 
 import data
 import metrics as met
+
+# def met_pixel_acc(ground_truth, results):
+#     results = data.preprocess_for_metrics(results)
+#     ground_truth = data.preprocess_for_metrics(ground_truth)
+
+#     length = ground_truth.shape[0]
+#     p_acc, m_acc, m_IU, fw_IU = 0, 0, 0, 0
+#     for i in range(length):
+#         p_acc += met.pixel_accuracy(results[i], ground_truth[i])
+#         m_acc += met.mean_accuracy(results[i], ground_truth[i])
+#         m_IU +=  met.mean_IU(results[i], ground_truth[i])
+#         fw_IU += met.frequency_weighted_IU(results[i], ground_truth[i])
+#     p_acc, m_acc, m_IU, fw_IU = p_acc/length, m_acc/length, m_IU/length, fw_IU/length
+
+#     return p_acc, m_acc, m_IU, fw_IU
+
+def met_pixel_acc(ground_truth, results):
+    results = data.preprocess_for_metrics(results)
+    ground_truth = data.preprocess_for_metrics(ground_truth)
+    length = ground_truth.shape[0]
+    p_acc = 0
+    for i in range(length):
+        p_acc += met.pixel_accuracy(results[i], ground_truth[i])
+    p_acc = p_acc/length
+
+    return p_acc
+
+def met_mean_acc(ground_truth, results):
+    results = data.preprocess_for_metrics(results)
+    ground_truth = data.preprocess_for_metrics(ground_truth)
+
+    length = ground_truth.shape[0]
+    m_acc = 0
+    for i in range(length):
+        m_acc += met.mean_accuracy(results[i], ground_truth[i])
+    m_acc = m_acc/length
+
+    return m_acc
+
+def met_mean_IU(ground_truth, results):
+    results = data.preprocess_for_metrics(results)
+    ground_truth = data.preprocess_for_metrics(ground_truth)
+
+    length = ground_truth.shape[0]
+    m_IU = 0
+    for i in range(length):
+        m_IU += met.mean_IU(results[i], ground_truth[i])
+    m_IU = m_IU/length
+
+    return m_IU
+
+def met_freq_w_IU(ground_truth, results):
+    results = data.preprocess_for_metrics(results)
+    ground_truth = data.preprocess_for_metrics(ground_truth)
+
+    length = ground_truth.shape[0]
+    fw_IU = 0
+    for i in range(length):
+        fw_IU += met.frequency_weighted_IU(results[i], ground_truth[i])
+    fw_IU = fw_IU/length
+
+    return fw_IU
 
 def fully_conv_InceptionV3():
     input_shape = (None, None, 3)
@@ -31,7 +95,7 @@ def fully_conv_InceptionV3():
     # plot_model(inception, 'inception_v3.png')
     print(inception.summary())
 
-def fully_conv_VGG16():
+def fully_conv_VGG16(metrics=None):
     input_shape = (None, None, 3)
     input_image = Input(shape=input_shape)
 
@@ -60,7 +124,7 @@ def fully_conv_VGG16():
     fcn8 = Conv2DTranspose(2, kernel_size=(16,16), strides=(8, 8), padding='same')(pre_fcn8)
 
     model = Model(inputs=[vgg16.input], outputs=[fcn8])
-    model.compile(optimizer=optimizers.Adam(lr=0.001), loss='binary_crossentropy')
+    model.compile(optimizer=optimizers.Adam(lr=0.001), loss='binary_crossentropy', metrics=metrics)
 
     return model
 
@@ -92,17 +156,29 @@ def fully_conv_net():
     return model
 
 class Segmenter(object):
-    def __init__(self):
-        self.model = fully_conv_VGG16()
+    def __init__(self, save_name=''):
+        self.metrics = [keras.metrics.binary_accuracy] #, met_pixel_acc, met_mean_acc, met_mean_IU, met_freq_w_IU]
+        self.model = fully_conv_VGG16(self.metrics)
+        self.save_name = save_name
 
-    def train(self, features, labels):
-        self.model.fit(features, labels, batch_size=1, epochs=1)
+        reduce_lr_plateau = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=2, verbose=1)
+        early_stop = EarlyStopping(monitor='val_loss', min_delta=0.001, patience=4, verbose=1)
+        model_checkpoint = ModelCheckpoint('saved_models/' + self.save_name + '_weights.epoch{epoch:02d}-acc{val_loss:.4f}.h5', monitor='val_loss', verbose=1, save_best_only=True)
 
-        save_path = 'saved_models/'+ time.strftime("%m-%dth_%H:%M:%S") +'.h5'
+        self.callbacks = [reduce_lr_plateau, early_stop, model_checkpoint]
+
+    def train(self, features, labels, val_features=None, val_labels=None):
+        if val_features is None or val_labels is None: val_data = None
+        else: val_data = (val_features, val_labels)
+
+        self.model.fit(features, labels, batch_size=1, epochs=40, validation_data=val_data, callbacks=self.callbacks)
+
+        if self.save_name: save_path = 'saved_models/'+ self.save_name
+        else: save_path = 'saved_models/'+ time.strftime("%m-%dth_%H:%M:%S") +'.h5'
         self.save_model(save_path)
 
     def infer(self, features):
-        results = self.model.predict(features)
+        results = self.model.predict(features, batch_size=1)
         
         binarized = np.zeros(results.shape)
         binarized[np.where(results[:,:,:,0] >= results[:,:,:,1])] = np.asarray([1, 0])
